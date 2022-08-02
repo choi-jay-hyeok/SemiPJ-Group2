684a8c8e556035d94e739f685d33d324ce40b5ff
seongkyeong jeong <105274896+elbibJeong@users.noreply.github.com>
2022-08-02 16:16:23 +0900

    test1

--- src/furnitureStore/Test1.java
+++ src/furnitureStore/Test1.java
@@ -3,6 +3,6 @@ package furnitureStore;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test1"); 
-		System.out.println("Test1"); 
+		System.out.println("Test 1"); 
 	}
 }
