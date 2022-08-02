9e3c3c4b68bd59065cf0863bbc3aa9b906f42bbb
seongkyeong jeong <105274896+elbibJeong@users.noreply.github.com>
2022-08-02 16:15:28 +0900

    commit

--- src/furnitureStore/Test1.java
+++ src/furnitureStore/Test1.java
@@ -2,6 +2,7 @@ package furnitureStore;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("Test1");
+		System.out.println("Test1"); 
+		System.out.println("Test1"); 
 	}
 }
